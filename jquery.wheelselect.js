5510f6d71f6cd876ad963a6697257de2024a3f7f
masum <miyaby@masum.jp>
2010-05-01 17:04:46 +0900

    update

--- jquery.wheelselect.js
+++ jquery.wheelselect.js
@@ -5,6 +5,7 @@ $.fn.extend({
     var $num = 5;
     var $center = Math.ceil($num/2);
     var $current = 0;
+    var $items = new Array();
     $this.css("position","relative");
     $this.css("height","68px");
     var makeList = function(idx) {
@@ -17,8 +18,7 @@ $.fn.extend({
       }
       pos = idx + param.list.length +1;
       for (var i=0;i<$num;i++) {
-        var p = idxArray[(pos-$center)+i];
-        $list.push(param.list[p]);
+        $list.push(idxArray[(pos-$center)+i]);
       }
       $current = idx;
     };
@@ -27,48 +27,40 @@ $.fn.extend({
     var css_size = ["0.6em","0.8em","1em","0.8em","0.6em"]
     var css_color = ["#ddd","#aaa","#000","#aaa","#ddd"];
     var makecss = function(i) {
-      return {"top":css_top[i],"left":css_left[i],
+      return {"top":css_top[i],"left":css_left[i],"display":"",
               "fontSize":css_size[i],"color":css_color[i]};
     }
-    var createItem =  function(item,i) {
+    var createItem =  function(item) {
       var div = $("<div class='wlist' style='position:absolute;font-size:12px;cursor:pointer'>");
-      return div.text($list[i].name)
-         .attr("alt",$list[i].value)
-         .css(makecss(i))
+      div.text(item.name)
+         .attr("alt",item.value)
          .bind("click",function(e) {
             param.onselect($(e.target).attr("alt"));
           });
+      div.hide();
+      return div;
     }
     var showList = function() {
-      for (i in $list) {
-        createItem($list[i],i).appendTo("#container");
+      for (var i=0;i<$num;i++) {
+        $items[$list[i]].css(makecss(i)).show();
+      }   
+    };
+    var animeList = function() {
+      for (var i=0;i<$num;i++) {
+        $items[$list[i]].show().animate(makecss(i),100);
       }   
     };
     var down = function() {
       var index = ($current==0)?param.list.length-1:$current-1;
       makeList(index);
-      $(".wlist").each(function(n,v) {
-        if (n==($num-1)) {
-          $(v).remove();
-        }else{
-          //$(v).animate(makecss(n+1),300);
-          $(v).css(makecss(n+1));
-        }
-      });
-      createItem($list[index],0).insertBefore($(".wlist")[0]);
+      $(".wlist").hide();
+      animeList();
     };
     var up = function() {
       var index = ($current>param.list.length)?0:$current+1;
       makeList(index);
-      $(".wlist").each(function(n,v) {
-        if (n==0) {
-          $(v).remove();
-        }else{
-          //$(v).animate(makecss(n-1),300);
-          $(v).css(makecss(n-1));
-        }
-      });
-      createItem($list[index],4).appendTo("#container");
+      $(".wlist").hide();
+      animeList();
     };
     $this.bind("DOMMouseScroll",function(e) {
       var d = e.detail;
@@ -78,7 +70,10 @@ $.fn.extend({
         up();
       }
     });
-    makeList(2);
+    for (i in param.list) {
+      $items[i] = createItem(param.list[i],i).hide().appendTo($this);
+    }
+    makeList(1);
     showList();
   }
 });
